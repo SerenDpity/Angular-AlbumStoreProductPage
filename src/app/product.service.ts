38bd09fd675d4990736800d44c1a199b4e007b79
EREN FAJARDO <sdptincorporated@gmail.com>
2023-03-13 01:12:54 +0800

    Displaying a List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,15 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 
+import { Product } from './product';
+
 import { Album } from './album';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json'
+  private _productsUrl = '../assets/products.json'
 
   constructor(private _http: Http) {
 
@@ -16,6 +19,10 @@ export class ProductService {
 
   getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json())
+  }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json())
   }
 
 }
